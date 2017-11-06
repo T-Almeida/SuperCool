2fd69691e6057b73271d71c4b7bfbc161f056cf8
Yuriy Muryn <murynyuriy@hotmail.com>
2017-11-06 19:21:07 +0000

    loader

---
+++ loader.js
@@ -0,0 +1,69 @@
+function Loader(){
+    this.gun1;
+    this.gun2;
+    this.gun3;
+
+    THREE.Loader.Handlers.add( /\.dds$/i, new THREE.DDSLoader() );
+
+    var progress = document.getElementById("loading");
+    var progressBar = document.getElementById("loadingBar");
+    var manager = new THREE.LoadingManager();
+    manager.onProgress = function ( url, itemsLoaded, itemsTotal ) {
+        progressBar.style.width = (itemsLoaded / itemsTotal * 100) + '%';
+    };
+    manager.onError = function ( url ) {
+        console.log( 'There was an error loading ' + url );
+    };
+
+    this.loadObj = function(model, obj, mtl, path) { 
+        var mtlLoader = new THREE.MTLLoader(manager);
+        mtlLoader.setPath(path);
+        mtlLoader.load( 
+            mtl, 
+            function( materials ) {
+                materials.preload();
+                var objLoader = new THREE.OBJLoader(manager);
+                objLoader.setMaterials( materials );
+                objLoader.setPath(path);
+                objLoader.load( 
+                    obj, 
+                    function ( object ) {
+                        model = object;
+                    } 
+                );
+            }
+        );
+    }
+
+    this.load3ds = function(model, tds, tga, path) {
+        var tgaLoader = new THREE.TGALoader(manager);
+        var glockTexture = tgaLoader.load(tga);
+        //3ds files dont store normal maps
+        var tdsLoader = new THREE.TDSLoader(manager);
+        tdsLoader.setPath(path);
+        tdsLoader.load( 
+            tds, 
+            function ( object ) {
+                object.children[0].material.map = glockTexture;
+                model = object;
+            }
+        );
+    }
+
+    this.loadModels = function() {
+        console.log("Game.loadModels()");
+
+
+        this.loadObj(gun1, 'M24_R_Low_Poly_Version_obj.obj', 'M24_R_Low_Poly_Version_obj.mtl', 'models/submachine/');
+        this.loadObj(gun2, 'gun.obj', 'gun.mtl', 'models/deagle/');
+        this.load3ds(gun3, 'models/glock/glock18c.3ds', 'models/glock/glock18c.tga','models/glock/');
+
+        while (gun1!=undefined || gun2!=undefined || gun3!=undefined);
+        progress.visible = false;
+    }
+
+
+}
+
+var loader = new Loader();
+loader.loadModels();
